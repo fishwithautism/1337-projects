0c6f9c9582f498bf9dc307d37e0567cbf23001b6
fishwithautism <ronemarie34@gmail.com>
2025-11-13 12:27:56 +0100

    Add files via upload

---
+++ ft_substr.c
@@ -0,0 +1,21 @@
+#include "libft.h"
+
+char	*ft_substr(char const *s, unsigned int start, size_t len)
+{
+	char	*sub;
+	size_t	s_len;
+	size_t	alloc_size;
+
+	if (!s)
+		return (NULL);
+	s_len = ft_strlen(s);
+	if (start >= s_len)
+		return (ft_strdup(""));
+	if (len > s_len - start)
+		len = s_len - start;
+	sub = (char *)malloc(sizeof(char) * (len + 1));
+	if (!sub)
+		return (NULL);
+	ft_strlcpy(sub, s + start, len + 1);
+	return (sub);
+}
