09d46e86b1c7ed07d5f08a89ce98892d08f0adac
fishwithautism <ronemarie34@gmail.com>
2025-11-15 15:33:43 +0100

    Fix null check logic in ft_strjoin function

--- libft/ft_strjoin.c
+++ libft/ft_strjoin.c
@@ -2,7 +2,7 @@ char *ft_strjoin(char const *s1, char const *s2)
 {
       size_t size;
       char *join;
-      if (!s1 || !s2)
+      if (!s1 && !s2)
           return (NULL);
       size = ft_strlen(s1) + ft_strlen(s2);
       join = malloc(size + 1);
