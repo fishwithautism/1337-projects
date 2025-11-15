1a5f5d458f444b3c4db615f82015746184901010
fishwithautism <ronemarie34@gmail.com>
2025-11-15 11:37:05 +0100

    Create ft_itoa.c

---
+++ libft/ft_itoa.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+
+static int	count_digits(int n)
+{
+	int	count;
+
+	count = 0;
+	if (n <= 0)
+		count = 1;
+	while (n != 0)
+	{
+		count++;
+		n /= 10;
+	}
+	return (count);
+}
+
+char	*ft_itoa(int n)
+{
+	char	*str;
+	int		len;
+	long	num;
+
+	num = n;
+	len = count_digits(num);
+	str = malloc(len + 1);
+	if (!str)
+		return (NULL);
+	str[len] = '\0';
+	if (num < 0)
+	{
+		str[0] = '-';
+		num = -num;
+	}
+	else if (num == 0)
+		str[0] = '0';
+	while (num > 0)
+	{
+		str[--len] = (num % 10) + '0';
+		num /= 10;
+	}
+	return (str);
+}
