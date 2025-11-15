d6aade69c8667d9974cab596af7a235222e93861
fishwithautism <ronemarie34@gmail.com>
2025-11-15 10:42:07 +0100

    Implement ft_strnstr function for substring search

---
+++ libft/ft_strnstr.c
@@ -0,0 +1,19 @@
+char	*ft_strnstr(const char *haystack, const char *needle, size_t n)
+{
+	size_t	i;
+	size_t	j;
+
+	if (!needle)
+		return ((char *)haystack);
+	i = 0;
+	while (haystack[i] && i < n)
+	{
+		j = 0;
+		while (needle[j] && haystack[i + j] == needle[j] && (i + j) < n)
+			j++;
+		if (needle[j] == '\0')
+			return ((char *)haystack + i);
+		i++;
+	}
+	return (NULL);
+}
